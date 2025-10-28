1ebb76d08d5da01de6451f8dfe8774ed84310353
Lorenzo Cherubin <53137532+lolelu@users.noreply.github.com>
2025-10-28 23:41:41 +0100

    Live preview

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,2 +1,5 @@
 //@ts-ignore
 export { enableDraftHandler as GET } from "@contentful/vercel-nextjs-toolkit/app-router";
+
+
+// https://www.contentful.com/developers/docs/tutorials/preview/live-updates/
